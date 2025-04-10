1b79d0a09ea2156e5abc9f66360d205bb613ae1c
z3itgeist <braga_gabriel@live.com>
2025-04-09 22:24:48 -0300

    pagina de cadastro

--- utilities.js
+++
@@ -1,7 +0,0 @@
-     //botão tela cadastro     
-     document.getElementById("btnCadastro").addEventListener("click", function() {
-        window.location.href = "cadastro.html";
-    });
-
-
-window.onload = listarMobile;
